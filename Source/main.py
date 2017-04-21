db77e4cc3fcb29891b4d330d864d5767e5599c16
dvh <dcutrigh@nm.org>
2017-04-21 16:54:57 -0500

    testing Bokeh server

--- Source/main.py
+++ Source/main.py
@@ -1,6 +1,6 @@
-from bokeh.layouts import column, widgetbox
+from bokeh.layouts import column, row, widgetbox
 from bokeh.models import ColumnDataSource
-from bokeh.models.widgets import Slider, DataTable, TableColumn, TextInput
+from bokeh.models.widgets import Slider, DataTable, TableColumn, TextInput, Select
 from bokeh.io import curdoc
 
 from Analysis_Tools import *
@@ -29,32 +29,12 @@ def update():
     }
 
 
-def update_text():
-    condition = query_text.value
-    print condition
-    current = DVH(cnx, dvh_condition=condition)
-    mrn = []
-    institutional_roi = []
-    roi_name = []
-    max_dose = []
-    for i in range(0, current.count):
-        mrn.append(current.mrn[i])
-        roi_name.append(current.roi_name[i])
-        institutional_roi.append(current.roi_institutional[i])
-        max_dose.append(current.max_dose[i])
-    source.data = {
-        'mrn'             : mrn,
-        'institutional_roi': institutional_roi,
-        'roi_name'           : roi_name,
-        'max_dose' : max_dose,
-    }
-
-
 slider = Slider(title="Max Dose", start=0, end=100, value=30, step=1)
 slider.on_change('value', lambda attr, old, new: update())
 
-query_text = TextInput(placeholder="e.g., institutional_roi = 'spinal_cord'")
-query_text.on_change('value', lambda attr, old, new: update_text())
+select1 = Select(title="Category", options=["mrn", "institutional_roi", "max_dose"])
+select2 = Select(title="Operator", options=["=", "<", ">", "like"])
+query_text = TextInput(title="condition", placeholder="enter condition")
 
 columns = [
     TableColumn(field="mrn", title="MRN"),
@@ -65,10 +45,10 @@ columns = [
 data_table = DataTable(source=source, columns=columns, width=800)
 
 controls = widgetbox(slider)
-text_in = widgetbox(query_text)
 table = widgetbox(data_table)
+query = widgetbox(select1, select2, query_text)
 
-curdoc().add_root(column(controls, text_in, table))
-curdoc().title = "Export CSV"
+curdoc().add_root(column(query, controls, table))
+curdoc().title = "Live Free or DICOM"
 
 update()
