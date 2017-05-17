adc0c4e63654e8d2ec9bfa47971f39da710c83c3
dvh <dcutrigh@nm.org>
2017-05-17 13:18:14 -0500

    moved recalculate_ages to a generic utilities.py

--- dvh/SQL_to_Python.py
+++ dvh/SQL_to_Python.py
@@ -1,6 +1,4 @@
 from DVH_SQL import *
-import datetime
-from dateutil.relativedelta import relativedelta
 
 
 class QuerySQL:
@@ -50,36 +48,3 @@ def get_unique_list(input_list):
             rtn_list_unique.append(value)
 
     return rtn_list_unique
-
-
-def recalculate_ages():
-
-    dvh_data = QuerySQL('Plans', "mrn != ''")
-    cnx = DVH_SQL()
-
-    for i in range(0, len(dvh_data.mrn)):
-        uid = dvh_data.study_instance_uid[i]
-        sim_study_date = dvh_data.sim_study_date[i].split('-')
-        birth_date = dvh_data.birth_date[i].split('-')
-
-        birth_year = int(birth_date[0])
-        birth_month = int(birth_date[1])
-        birth_day = int(birth_date[2])
-        birth_date_obj = datetime.datetime(birth_year, birth_month, birth_day)
-
-        sim_study_year = int(sim_study_date[0])
-        sim_study_month = int(sim_study_date[1])
-        sim_study_day = int(sim_study_date[2])
-        sim_study_date_obj = datetime.datetime(sim_study_year,
-                                               sim_study_month,
-                                               sim_study_day)
-
-        if sim_study_date == '1800-01-01':
-            age = '(NULL)'
-        else:
-            age = relativedelta(sim_study_date_obj, birth_date_obj).years
-
-        condition = "study_instance_uid = '" + uid + "'"
-        cnx.update('Plans', 'age', str(age), condition)
-
-    cnx.cnx.close()
