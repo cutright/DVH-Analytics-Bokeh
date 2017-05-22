cb138ef11a10e2b68e4d263dad5b6d5d997670bd
dvh <dcutrigh@nm.org>
2017-05-22 18:33:10 -0500

    installation notes

---
+++ install-python-packages_win.bat
@@ -0,0 +1,11 @@
+pip install numpy
+pip install pydicom
+pip install matplotlib
+pip install --ignore-installed six
+pip install dicompyler-core
+pip install Jinja2
+pip install Requests
+pip install Tornado==4.4.2
+pip install PyYaml
+pip install bokeh
+pip install psycopg2
