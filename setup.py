5d1a3a1186d1a3ce12e0a12c26d0fc4849975bf9
cutright <dcutrigh@nm.org>
2017-05-28 22:50:43 -0500

    pypi setup

--- setup.py
+++ setup.py
@@ -1,9 +1,25 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+
+requires = [
+    'pydicom >= 0.9.9',
+    'matplotlib >= 1.3.0',
+    'six >= 1.5',
+    'dicompyler - core == 0.5.2',
+    'Jinja2',
+    'Requests',
+    'Tornado == 4.4.2',
+    'PyYaml',
+    'bokeh == 0.12.5',
+    'python-dateutil',
+    'psycopg2',
+    'fuzzywuzzy[speedup]',
+]
 
 setup(
     name='dvh-analytics',
-    packages=['dvh-analytics'],
-    version='0.0.1',
+    include_package_data=True,
+    packages=find_packages(),
+    version='0.0.3',
     description='Create a SQL database of DVHs',
     author='Dan Cutright',
     author_email='dan.cutright@gmail.com',
