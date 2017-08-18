01f65317975431a2d301c38a7afdd118396c1e61
cutright <dcutrigh@nm.org>
2017-08-18 10:24:23 -0500

    update

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     name='dvh-analytics',
     include_package_data=True,
     packages=find_packages(),
-    version='0.1.23',
+    version='0.1.25',
     description='Create a database of DVHs, views with Bokeh',
     author='Dan Cutright',
     author_email='dan.cutright@gmail.com',
