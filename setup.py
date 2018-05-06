c447e468595efed11209e9c82ebb9f90ed3ae13c
cutright <dan.cutright@gmail.com>
2018-05-06 00:02:57 -0400

    requirement bug fix

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup, find_packages
 
 requires = [
     'scipy',
-    'numpy >= 1.2',
+    'numpy',
+    'fuzzywuzzy',
     'pydicom >= 0.9.9',
     'matplotlib',
     'six >= 1.5',
@@ -24,7 +25,7 @@ setup(
     name='dvh-analytics',
     include_package_data=True,
     packages=find_packages(),
-    version='0.3.11',
+    version='0.3.13',
     description='Create a database of DVHs, views with Bokeh',
     author='Dan Cutright',
     author_email='dan.cutright@gmail.com',
