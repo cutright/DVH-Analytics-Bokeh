7560c5bf1eac658a7095a7ce13e688a105080f2c
cutright <dan.cutright@gmail.com>
2018-12-26 11:28:23 -0500

    tweak

--- dvh/update_sys_path.py
+++ dvh/update_sys_path.py
@@ -1,3 +1,3 @@
 import sys
-import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+from os.path import dirname, realpath
+sys.path.append(dirname(dirname(realpath(__file__))))
